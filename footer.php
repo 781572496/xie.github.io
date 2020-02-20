06692500c3edb4e98fb89831e370079c296efb97
781572496 <60023408+781572496@users.noreply.github.com>
2020-02-20 19:20:18 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,5 @@
+<script async src="path/to/waifu-tips.js"></script>
+<script src="path/to/live2d.js"></script>
+<script type="text/javascript">
+    loadlive2d("live2d", "path/to/model.json");
+</script>
